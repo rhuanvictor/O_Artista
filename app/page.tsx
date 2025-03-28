dd2520dd38e4db4cb344f59b6bf2a1d83a6ba81f
Rhuan <87355160+rhuanvictor@users.noreply.github.com>
2025-03-28 20:23:20 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { Link } from "@nextui-org/link";
-//import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+
+
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import Badge from "@/components/chip/chip";
